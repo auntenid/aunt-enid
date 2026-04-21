b02f97c992bd41bbf12364d5c72dacd62a9ac977
twiinarides <twiinarides@gmail.com>
2026-04-21 15:57:50 +0300

    Fix media serving in production: use re_path+serve instead of static() which returns empty list when DEBUG=False

--- aunt_enid_campaign/urls.py
+++ aunt_enid_campaign/urls.py
@@ -15,17 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
+    # Serve media files in both DEBUG and production (re_path bypasses Django's DEBUG-only restriction)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# Serve media files (in both DEBUG and production since we are not using S3)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Serve static files only in DEBUG (WhiteNoise handles them in production)
 if settings.DEBUG:
